8c1f4b3b34f94a0ec4ee135a18f50e81f582dcc8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-04 10:08:14 +0100

    use generators

--- R/is.R
+++ R/is.R
@@ -1,17 +1,20 @@
+#' @include oo.R
+NULL
+
 #' Check if the condition is of the appropriate type
 #' @param x The object to check
 #' @param ... Ignored
 #' @export
-is.condition <- function(x, ...) inherits(x, "condition")
+is.condition <- Condition$is
 
 #' @rdname is.condition
 #' @export
-is.error <- function(x, ...) is.condition(x) && inherits(x, "error")
+is.error <- Error$is
 
 #' @rdname is.condition
 #' @export
-is.warning <- function(x, ...) is.condition(x) && inherits(x, "warning")
+is.warning <- Warning$is
 
 #' @rdname is.condition
 #' @export
-is.message <- function(x, ...) is.condition(x) && inherits(x, "message")
+is.message <- Message$is
--- R/new.R
+++ R/new.R
@@ -1,31 +1,21 @@
+#' @include oo.R
+NULL
+
 #' Create a condition
 #' @export
 #' @param class The classes from which the condition should inherit, in addition
 #'   to the default classes
 #' @inheritParams base::simpleCondition
-new_condition <- function(message, call = NULL, class = NULL) {
-  class <- unique(c(class, "condition"), fromLast = TRUE)
-  structure(list(message = as.character(message), call = call),
-            class = class)
-}
+new_condition <- Condition$new
 
 #' @rdname condition
 #' @export
-new_error <- function(message, call = NULL, class = NULL) {
-  class <- unique(c(class, "error"), fromLast = TRUE)
-  new_condition(message = message, call = call, class = class)
-}
+new_error <- Error$new
 
 #' @rdname condition
 #' @export
-new_warning <- function(message, call = NULL, class = NULL) {
-  class <- unique(c(class, "warning"), fromLast = TRUE)
-  new_condition(message = message, call = call, class = class)
-}
+new_warning <- Warning$new
 
 #' @rdname condition
 #' @export
-new_message <- function(message, call = NULL, class = NULL) {
-  class <- unique(c(class, "message"), fromLast = TRUE)
-  new_condition(message = message, call = call, class = class)
-}
+new_message <- Message$new
